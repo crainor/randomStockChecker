8fb74536aeb03e5f615790b4b7be4af50a5c9eb3
b8018716 <58822432+camscott99@users.noreply.github.com>
2022-05-26 12:06:09 +0100

    Update Random Stock Check.py

--- Random Stock Check.py
+++ Random Stock Check.py
@@ -1,5 +1,12 @@
+from cgi import test
+from inspect import iscode
+from setuptools import extension
+from fileinput import filename
+from itertools import count
 import random
 import datetime
+from typing import Counter
+import os
 
 #A simple script to ensure a truly random choice when doing the daily random
 # stock takes. A work in progress at the moment. Not all locations added
@@ -7,10 +14,46 @@ import datetime
 
 #C Reyner 2022
 
+# Functions - CScott
+#Small function that will add (1) on to the end to stop files being created with the same name.
+def createFilePath(path):
+    file, extension = os.path.splitext(path)
+    counter = 1
+    while os.path.exists(path):
+        path = file + " (" + str(counter) + ") " + extension
+        counter += 1
+    
+    return path
+
+#Function to check if an input is either "Y" "N". 
+def yesNoCheck(input):
+    if input == "Y" or input == 'N' or input == "n"  or input == 'y':
+        return True
+    else:
+        return False
+
+
+
 #date n time
 today = datetime.datetime.now()
 fileName = "Random Stock Take - " + today.strftime("%d " ) + today.strftime("%b") + ".txt"
+fileName = createFilePath(fileName)
+#numLocations = input("How many locations would you like to check?")
 
+while True:
+    try:
+        numLocations = int(input("How many locations would you like to check?"))
+    except ValueError:
+        print("Please enter a number")
+        continue
+    if numLocations < 0:
+        print("Please enter a positive number")
+        continue
+    if numLocations > 20:
+        print("There are only a maximum of 20 spots for a stock check")
+    else:
+        break
+    
 
 # Racking Locations
 
@@ -24,90 +67,59 @@ rackingArrayLoc = ["A1", "A2" , "A3", "A4", "A5", "A6", "A7", "A8", "A9",
 
 rackingLetters = ["A", "B", "C", "D"]
 
-#isCorrect checks - Variables to hold Y/N chars
 
-isCorrect1  = ("")
-isCorrect2  = ("")
-isCorrect3  = ("")
-isCorrect4  = ("")
-isCorrect5  = ("")
-isCorrect6  = ("")
-isCorrect7  = ("")
-isCorrect8  = ("")
-isCorrect9  = ("")
-isCorrect10  = ("")
                  
 #Need to add rest of racking spots + letters (in a seperate array then use concancation to combine)
 
-randomCheck1 = random.choice(rackingArrayLoc)
-randomCheck2 = random.choice(rackingArrayLoc)
-randomCheck3 = random.choice(rackingArrayLoc)
-randomCheck4 = random.choice(rackingArrayLoc)
-randomCheck5 = random.choice(rackingArrayLoc)
-randomCheck6 = random.choice(rackingArrayLoc)
-randomCheck7 = random.choice(rackingArrayLoc)
-randomCheck8 = random.choice(rackingArrayLoc)
-randomCheck9 = random.choice(rackingArrayLoc)
-randomCheck10 = random.choice(rackingArrayLoc)
+randomCheck= []
+randomLetter = []
+isCorrect = []
 
-randomLetter1 = random.choice(rackingLetters)
-randomLetter2 = random.choice(rackingLetters)
-randomLetter3 = random.choice(rackingLetters)
-randomLetter4 = random.choice(rackingLetters)
-randomLetter5 = random.choice(rackingLetters)
-randomLetter6 = random.choice(rackingLetters)
-randomLetter7 = random.choice(rackingLetters)
-randomLetter8 = random.choice(rackingLetters)
-randomLetter9 = random.choice(rackingLetters)
-randomLetter10 = random.choice(rackingLetters)
+for x in range(numLocations):
+    isCorrect.append("NULL")
+    randomCheck.insert(x, random.choice(rackingArrayLoc))
+    randomLetter.insert(x, random.choice(rackingLetters))
+
 print(datetime.datetime.now())
-print("This program will generate 10 random locations in order to eliminate bias and ensure a 'Truly Random' choice is selected")
-print("The 10 Locations are:")
+print("This program will generate random locations in order to eliminate bias and ensure a 'Truly Random' choice is selected")
+print("The Locations are:")
 
 
-print(randomCheck1 + "-" + randomLetter1)
-print(randomCheck2 + "-" + randomLetter2)
-print(randomCheck3 + "-" + randomLetter3)
-print(randomCheck4 + "-" + randomLetter4)
-print(randomCheck5 + "-" + randomLetter5)
-print(randomCheck6 + "-" + randomLetter6)
-print(randomCheck7 + "-" + randomLetter7)
-print(randomCheck8 + "-" + randomLetter8)
-print(randomCheck9 + "-" + randomLetter9)
-print(randomCheck10 + "-" + randomLetter10)
+for i in range(numLocations):
+    print(randomCheck[i] + "-" + randomLetter[i])
+
 
 #isCorrect checks
 
 
 print("Did the locations hold the correct stock? Please answer Y / N")
 
-isCorrect1 = input(randomCheck1 + "-" + randomLetter1 + ":")
-isCorrect2  = input(randomCheck2 + "-" + randomLetter2 + ":")
-isCorrect3  = input(randomCheck3 + "-" + randomLetter3 + ":")
-isCorrect4  = input(randomCheck4 + "-" + randomLetter4 + ":")
-isCorrect5  = input(randomCheck5 + "-" + randomLetter5 + ":")
-isCorrect6  = input(randomCheck6 + "-" + randomLetter6 + ":")
-isCorrect7  = input(randomCheck7 + "-" + randomLetter7 + ":")
-isCorrect8  = input(randomCheck8 + "-" + randomLetter8 + ":")
-isCorrect9  = input(randomCheck9 + "-" + randomLetter9 + ":")
-isCorrect10  = input(randomCheck10 + "-" + randomLetter10 + ":")
+
+
+for i in range(numLocations):
+    
+    while True:
+        isCorrect[i] = input(randomCheck[i] + "-" + randomLetter[i] + ":").upper()
+       
+        if yesNoCheck(isCorrect[i]) != True:
+            print(isCorrect[i])
+            print("\n ERROR Please enter Y or N \n")
+            continue
+        else:
+            print("Thanks")
+        break
+
 
 #Print log file for digital records
 
 
 f = open(fileName, "x")
-f.write(randomCheck1 + "-" + randomLetter1+ ": " + isCorrect1 + "\n")
-f.write(randomCheck2 + "-" + randomLetter2 + ": " + isCorrect2 + "\n")
-f.write(randomCheck3 + "-" + randomLetter3 + ": " + isCorrect3 + "\n")
-f.write(randomCheck4 + "-" + randomLetter4 + ": " + isCorrect4 + "\n")
-f.write(randomCheck5 + "-" + randomLetter5 + ": " + isCorrect5 + "\n")
-f.write(randomCheck6 + "-" + randomLetter6 + ": " + isCorrect6 + "\n")
-f.write(randomCheck7 + "-" + randomLetter7 + ": " + isCorrect7 + "\n")
-f.write(randomCheck8 + "-" + randomLetter8 + ": " + isCorrect8 + "\n")
-f.write(randomCheck9 + "-" + randomLetter9 + ": " + isCorrect9 + "\n")
-f.write(randomCheck10 + "-" + randomLetter10 + ": " + isCorrect10 + "\n")
-
+for i in range(numLocations):
+    f.write(randomCheck[i] + "-" + randomLetter[i]+ ": " + isCorrect[i] + "\n")
+f.close()
 
 print("The results have been saved to the directory.")
 print("Press enter to exit.")
 input()
+
+
